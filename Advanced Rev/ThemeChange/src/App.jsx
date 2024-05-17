b6da300ba0c29549e36cfe3ca7f38fdf80b1fb58
Sumitsh28 <sumit.off28@gmail.com>
2024-05-17 09:55:24 +0530

    Theme done

--- Advanced Rev/ThemeChange/src/App.jsx
+++ Advanced Rev/ThemeChange/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import Starrating from "./components/Starrating";
 import ThemeChange from "./components/ThemeChange";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center mt-96 gap-10">
+      <div className="flex flex-col justify-center items-center gap-10">
         <ThemeChange />
       </div>
     </>
--- Advanced Rev/ThemeChange/src/components/ThemeChange.jsx
+++ Advanced Rev/ThemeChange/src/components/ThemeChange.jsx
@@ -1,5 +1,30 @@
+import useLoacalStorage from "./UseLocalStorage";
+
 const ThemeChange = () => {
-  return <div className="flex flex-row text-3xl">Theme</div>;
+  const [theme, setTheme] = useLoacalStorage("theme", "dark");
+
+  const handleThemeChange = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+    console.log(theme);
+  };
+
+  return (
+    <div
+      className={`flex flex-col text-xl items-center w-screen h-screen ${
+        theme === "light" ? "bg-gray-300" : "bg-gray-800"
+      } bg-black`}
+    >
+      <p>{theme}</p>
+      <button
+        onClick={handleThemeChange}
+        className={`p-5 ${
+          theme === "light" ? "bg-gray-800" : "bg-gray-300"
+        } rounded-full`}
+      >
+        Change Theme
+      </button>
+    </div>
+  );
 };
 
 export default ThemeChange;
